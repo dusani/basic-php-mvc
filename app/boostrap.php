883427a1597bf67b09933b904d5f60ef53cd4144
Daniel Usani <daniel.e.usani@gmail.com>
2018-05-03 13:48:00 -0400

    Added autoloader to boostrap.php

--- app/boostrap.php
+++ app/boostrap.php
@@ -1,5 +1,13 @@
 <?php
+  // Require Config
+  require_once 'config/config.php';
+
   // Require Libraries
-  require_once 'libraries/Core.php';
-  require_once 'libraries/Controller.php';
-  require_once 'libraries/Database.php';
+  // require_once 'libraries/Core.php';
+  // require_once 'libraries/Controller.php';
+  // require_once 'libraries/Database.php';
+
+  // Autoload Core Libraries
+  spl_autoload_register(function($className){
+    require_once 'libraries/' . $className . '.php';
+  });
