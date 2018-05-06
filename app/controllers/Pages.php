1a3d24d2cb6d02d143901d04f3ad5446e524c8b1
Daniel Usani <daniel.e.usani@gmail.com>
2018-05-06 13:15:31 -0400

    Added PDO Database Class

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -2,18 +2,27 @@
   class Pages extends Controller{
     public function __construct() {
       // echo 'Pages loaded';
+      $this->postModel = $this->model('Post');
     }
 
     public function index() {
       // $this->view('hello');
+      $posts = $this->postModel->getPosts();
+
       $data = [
-        'title' => 'Welcome'
+        'title' => 'Welcome',
+        'posts' => $posts
       ];
+
 
       $this->view('pages/index', $data);
     }
 
     public function about() {
-      $this->view('pages/about');
+      $data = [
+        'title' => 'About Us'
+      ];
+
+      $this->view('pages/about', $data);
     }
   }
